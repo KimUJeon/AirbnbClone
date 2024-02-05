885401b818b589cf876ff4acdda37797c51afe71
KimUJeon <kinfe_411@naver.com>
2024-02-05 21:12:22 +0900

    #2.3 super() 강의 수강 완료
    함수를 상속 받기 위해서는 super를 통해 해당 함수를 동작시켜주는 액션이 필요함

--- oop_test.py
+++ oop_test.py
@@ -1,10 +1,21 @@
-class Player:
-    def __init__(self, name, xp):
+class Human:
+    def __init__(self, name):
         self.name = name
-        self.xp = xp
 
     def say_hello(self):
-        print(self.name, self.xp)
+        print(f"hello my name is {self.name}")
 
-nico = Player("nico", 1000)
-print(nico.name, nico.xp)
+class Player(Human):
+    def __init__(self, name, xp):
+        super().__init__(name)
+        self.xp = xp
+
+class Fan(Human):
+    def __init__(self, name, fav_team):
+        super().__init__(name)
+        self.fav_team = fav_team
+
+nico_player = Player("nico", 10)
+nico_player.say_hello()
+nico_fan = Fan("nico_fan", "dontknow")
+nico_fan.say_hello()
