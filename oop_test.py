2075ae54c5ac7440e9eee6a9f1df350e8dffc72e
KimUJeon <kinfe_411@naver.com>
2024-02-06 21:48:47 +0900

    #2.5 dir 강의 수강 완료

--- oop_test.py
+++ oop_test.py
@@ -19,15 +19,27 @@
 # nico_player.say_hello()
 # nico_fan = Fan("nico_fan", "dontknow")
 # nico_fan.say_hello()
+#
+# class Dog:
+#     def woof(self):
+#         print("woof woof")
+#
+# class Beagle(Dog):
+#     def woof(self):
+#         super().woof()
+#         print("super woof")
+#
+# beagle = Beagle()
+# beagle.woof()
 
 class Dog:
-    def woof(self):
-        print("woof woof")
+    def __init__(self, name):
+        self.name = name
+    def __str__(self):
+        return f"Dog: {self.name}"
+    def __getattribute__(self, name):
+        print(f"they want to get {name}")
+        return "😂"
 
-class Beagle(Dog):
-    def woof(self):
-        super().woof()
-        print("super woof")
-
-beagle = Beagle()
-beagle.woof()
+jia = Dog("jia")
+print(jia.name)
