899da98f6261d6807a6795ddd0c9e44c6795884a
KimUJeon <kinfe_411@naver.com>
2024-02-19 13:29:37 +0900

    #4.1 Migrations
    DB 모델 수정시 반드시 해야하는 makemigrations 와 migrate에 대해 학습

--- houses/admin.py
+++ houses/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import House
 
-# Register your models here.
+# ModelAdmin과 동일한 class가 생성된 것
+@admin.register(House)
+class HouseAdmin(admin.ModelAdmin):
+    pass
