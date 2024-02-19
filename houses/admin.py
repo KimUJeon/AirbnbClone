bbf07948bc94c46b6259e4258edefab5bda37110
KimUJeon <kinfe_411@naver.com>
2024-02-19 14:37:12 +0900

    #4.3 Admin
    Admin 관리 페이지에서 설정 가능한 간단한 설정과 기타 사용법에 대해 학습함

--- houses/admin.py
+++ houses/admin.py
@@ -4,4 +4,7 @@ from .models import House
 # ModelAdmin과 동일한 class가 생성된 것
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["name", "price_per_night", "address", "pets_allowed"]
+    list_filter = ["price_per_night", "pets_allowed"]
+    # __startswith 을 붙이면 해당 단어로 시작하는 데이터만 탐색함
+    search_fields = ["address"]
