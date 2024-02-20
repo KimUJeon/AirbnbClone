fe1dfdce0bd7702c0eef50abfc2e78df74db8602
KimUJeon <kinfe_411@naver.com>
2024-02-20 10:37:21 +0900

    #5.1 Custom Model
    커스텀 사용자 모델을 활용하여 관리자 페이지 데이터 변경과 모델 구조 변경에 대해 학습

--- houses/admin.py
+++ houses/admin.py
@@ -4,13 +4,6 @@ from .models import House
 # ModelAdmin과 동일한 class가 생성된 것
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
-    fields = ("name",
-              "address",
-              ("price_per_night", "pets_allowed"),
-              )
+
     list_display = ("name", "price_per_night", "address", "pets_allowed")
     list_filter = ("price_per_night", "pets_allowed")
-    # __startswith 을 붙이면 해당 단어로 시작하는 데이터만 탐색함
-    search_fields = ("address",)
-    list_display_links = ("name", "address")
-    list_editable = ("pets_allowed",)
