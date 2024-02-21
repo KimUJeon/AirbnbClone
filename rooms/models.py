b9725612a60e1afc3265c59b7117dd7093019a4b
KimUJeon <kinfe_411@naver.com>
2024-02-21 15:14:23 +0900

    #6.4 Rooms Admin
    이전에 만들었던 created, updated 데이터를 보여주는 방법에 대해 학습(데이터 테이블을 따로 만들지 않았음)

--- rooms/models.py
+++ rooms/models.py
@@ -13,6 +13,10 @@ class Room(CommonModel):
         PRIVATE_ROOM = ("private_room", "Private Room")
         SHARED_ROOM = ("shared_room", "Shared Room")
 
+    name = models.CharField(
+        max_length=180,
+        default="",
+    )
     country = models.CharField(
         max_length=50,
         default="한국",
@@ -43,6 +47,9 @@ class Room(CommonModel):
         "rooms.Amenity",
     )
 
+    def __str__(self):
+        return self.name
+
 
 class Amenity(CommonModel):
     """Amenity Definition"""
@@ -55,3 +62,9 @@ class Amenity(CommonModel):
         default="",
         blank=True,
     )
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Amenities"
