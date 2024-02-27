0148868a707940b473c745ada4a858cb169a93ad
KimUJeon <kinfe_411@naver.com>
2024-02-27 17:28:05 +0900

    #8.3 Custom Filters
    관계를 이용한 필터가 가능함, WordFilter 클래스를 이용하면 사용자가 원하는 형식으로 필터링이 가능함(대소문자 구분없이 검색해주는 것 같음)

--- reviews/admin.py
+++ reviews/admin.py
@@ -2,6 +2,25 @@ from django.contrib import admin
 from .models import Review
 
 
+class WordFilter(admin.SimpleListFilter):
+    title = "Filter by Words!"
+    parameter_name = "word"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("good", "Good"),
+            ("great", "Great"),
+            ("awesome", "Awesome"),
+        ]
+
+    def queryset(self, request, reviews):
+        word = self.value()
+        if word:
+            return reviews.filter(payload__contains=word)
+        else:
+            return reviews
+
+
 @admin.register(Review)
 class Review(admin.ModelAdmin):
     list_display = (
@@ -9,4 +28,10 @@ class Review(admin.ModelAdmin):
         "payload",
     )
 
-    list_filter = ("rating",)
+    list_filter = (
+        "rating",
+        "user__is_host",
+        "room__category",
+        "room__pet_friendly",
+        WordFilter,
+    )
