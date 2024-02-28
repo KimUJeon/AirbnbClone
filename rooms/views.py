51bdc22fbc108e04b4f9480f11306147a4845a73
KimUJeon <kinfe_411@naver.com>
2024-02-28 13:20:36 +0900

    #9.2 URL Arguments
    URL 패턴 기능중 url에 있는 변수 값<타입: 변수명> 을 통해 값을 받아들이고 이를 활용하는 방법에 대해 학습함

--- rooms/views.py
+++ rooms/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def say_hello(request):
-    return HttpResponse("hello")
+def see_all_rooms(request):
+    return HttpResponse("See all room")
+
+
+def see_one_room(request, room_id):
+    return HttpResponse(f"See one room: {room_id}")
