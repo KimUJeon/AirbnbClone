3b98d05eab638ee55de99d3696d62159e31708c5
KimUJeon <kinfe_411@naver.com>
2024-02-29 10:51:18 +0900

    #10.2 api_view
    장고 rest_framework를 이용해 깔끔한 결과물을 출력할 수 있고 이를 적용하는 방법에 대해 학습함

--- categories/views.py
+++ categories/views.py
@@ -1,12 +1,14 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Category
 
 
+@api_view()
 def categories(request):
     all_categories = Category.objects.all()
-    return JsonResponse(
+    return Response(
         {
             "OK": True,
-            "categories": all_categories,
+            "categories": Category.objects.all(),
         }
     )
