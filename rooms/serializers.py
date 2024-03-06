170666c1ed73381e7a16ef0f2ec2b6fc2a140bd8
KimUJeon <kinfe_411@naver.com>
2024-03-06 14:09:22 +0900

    #11.11 SerializerMethodField
    Serializer가 다른 메서드에서 가공한 값을 가져와 사용하는 것이 가능하다는 것을 알 수 있었음
    
    이 부분은 개발자가 어떤 값이든 원하면 가공해서 넣을 수 있음을 의미함

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from .models import Amenity, Room
 from users.serializers import TinyUserSerializer
 from categories.serializers import CategorySerializer
@@ -22,10 +22,14 @@ class RoomDetailSerializer(ModelSerializer):
     category = CategorySerializer(
         read_only=True,
     )
+    rating = SerializerMethodField()
 
     class Meta:
         model = Room
         fields = "__all__"
+
+    def get_rating(self, room):
+        return room.rating()
 
 
 class RoomListSerializer(ModelSerializer):
@@ -37,4 +41,10 @@ class RoomListSerializer(ModelSerializer):
             "country",
             "city",
             "price",
+            "rating",
         )
+
+    rating = SerializerMethodField()
+
+    def get_rating(self, room):
+        return room.rating()
