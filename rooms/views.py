27e7d055d1f98596050ed0f7dc75b3fe3902072a
KimUJeon <kinfe_411@naver.com>
2024-03-06 13:54:09 +0900

    #11.10 Delete Rooms
    사용자의 인증 상태나 게시물에 대한 소유권한을 확인하고 이를 이용해 동작 가능한 범위를 제한하는 방법에 대해 학습함

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.response import Response
-from rest_framework.exceptions import NotFound, NotAuthenticated, ParseError
+from rest_framework.exceptions import (
+    NotFound,
+    NotAuthenticated,
+    ParseError,
+    PermissionDenied,
+)
 from django.db import transaction
 from .models import Amenity, Room
 from categories.models import Category
@@ -107,3 +112,33 @@ class RoomDetail(APIView):
         room = self.get_object(pk)
         serializer = RoomDetailSerializer(room)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        room = self.get_object(pk)
+        if not request.user.is_authenticated:
+            raise NotAuthenticated
+        if room.owner != request.user:
+            raise PermissionDenied
+
+        serializer = RoomDetailSerializer(
+            room,
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            updated_room = serializer.save()
+            return Response(
+                RoomDetailSerializer(updated_room).data,
+            )
+        else:
+            return Response(serializer.errors)
+
+    def delete(self, request, pk):
+        room = self.get_object(pk)
+        if not request.user.is_authenticated:
+            raise NotAuthenticated
+        if room.owner != request.user:
+            raise PermissionDenied
+
+        room.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
