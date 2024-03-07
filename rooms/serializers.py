2403c815a8817f1cd6a00424f897a264615381c0
KimUJeon <kinfe_411@naver.com>
2024-03-07 17:31:41 +0900

    #11.21 is_liked
    is_like 상태를 쉽게 찾는 방법(데이터 안에 있는 데이터를 찾는)에 대해 학습함

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -4,6 +4,7 @@ from users.serializers import TinyUserSerializer
 from reviews.serializers import ReviewSerializer
 from categories.serializers import CategorySerializer
 from medias.serializers import PhotoSerializer
+from wishlists.models import Wishlist
 
 
 class AmenitySerializer(ModelSerializer):
@@ -26,6 +27,7 @@ class RoomDetailSerializer(ModelSerializer):
     )
     rating = SerializerMethodField()
     is_owner = SerializerMethodField()
+    is_liked = SerializerMethodField()
     photos = PhotoSerializer(
         many=True,
         read_only=True,
@@ -41,6 +43,10 @@ class RoomDetailSerializer(ModelSerializer):
     def get_is_owner(self, room):
         request = self.context["request"]
         return room.owner == request.user
+
+    def get_is_liked(self, room):
+        request = self.context["request"]
+        return Wishlist.objects.filter(user=request.user, rooms__pk=room.pk).exists()
 
 
 class RoomListSerializer(ModelSerializer):
