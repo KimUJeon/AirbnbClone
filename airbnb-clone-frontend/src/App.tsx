e02b4d484db024580e4cca40488e20757d0b27eb
KimUJeon <kinfe_411@naver.com>
2024-04-29 14:21:20 +0900

    Front-end Init settings

--- airbnb-clone-frontend/src/App.tsx
+++ airbnb-clone-frontend/src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Text } from "@chakra-ui/react";
 
 function App() {
-  return <div />;
+  return (
+    <div>
+      <Text color={"red.500"} fontSize={"6xl"}>
+        {" "}
+        It works!{" "}
+      </Text>
+    </div>
+  );
 }
 
 export default App;
